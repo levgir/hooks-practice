ae53851124b830a2c85023479144ffe0d7091aa9
Levi Giraud <levigiraud@Levis-MacBook-Air.local>
2020-04-15 21:25:45 -0600

    began building functional App component and set up hook to usestate

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [resource, setResource] = useState("posts");
+
+  console.log(resource);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      {resource}
+    </div>
+  );
+};
+
+export default App;
